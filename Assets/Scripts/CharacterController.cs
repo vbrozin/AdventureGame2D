09e255df2e121b1d59c422562210f675f8d4467a
Valentin BROZIN <valentinbrozin@gmail.com>
2017-01-21 22:30:54 +0100

    first commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float moveForce = 365f;			// Amount of force added to move the player left and right.
+    public float maxSpeed = 5f;             // The fastest the player can travel in the x axis.
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        // Cache the horizontal input.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(h, v);
+
+        // The Speed animator parameter is set to the absolute value of the horizontal input.
+        //anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (move.magnitude != 0)
+        {
+            if (move.magnitude * GetComponent<Rigidbody2D>().velocity.magnitude < maxSpeed)
+                // ... add a force to the player.
+                GetComponent<Rigidbody2D>().AddForce(move * moveForce);
+
+            if (GetComponent<Rigidbody2D>().velocity.magnitude > maxSpeed)
+                GetComponent<Rigidbody2D>().velocity = move * maxSpeed;
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+        // If the input is moving the player right and the player is facing left...
+        Flip(move);
+
+    }
+
+    private void Flip(Vector2 move)
+    {
+        //TODO
+    }
+}
---
+++ Assets/Scripts/EnnemieBehaviour.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemieBehaviour : MonoBehaviour {
+
+    public List<Vector2> pattern;
+
+    public float detectionRadius = 2f;
+
+    public float speed = 5f;
+
+    private float currentDistance = 0f;
+
+    private Vector2 currentTarget;
+
+    private float valid = 0.1f;
+
+    private GameObject hero = null;
+
+    private IEnumerator<Vector2> enumerator;
+
+    private bool isBumping = false;
+
+    // Use this for initialization
+    void Start () {
+        // Insert initial position to pattern
+       Vector2  initialPosition = gameObject.transform.position;
+        pattern.Insert(0, initialPosition);
+
+        // Initialise Enumerator and start to next position
+       enumerator = pattern.GetEnumerator();
+        enumerator.MoveNext();
+       currentTarget = enumerator.Current;
+    }
+
+    // Update is called once per frame
+    void Update() { 
+        if (!isBumping) { 
+        if (seeHero())
+        {
+            currentTarget = hero.transform.position;
+            Vector2 delta = currentTarget - new Vector2(transform.position.x, transform.position.y);
+            GetComponent<Rigidbody2D>().velocity = delta.normalized * speed;
+
+        }
+        else if (pattern.Count > 1)
+        {
+            Vector2 delta = currentTarget - new Vector2(transform.position.x, transform.position.y);
+
+            if (delta.magnitude < valid)
+            {
+                if (!enumerator.MoveNext())
+                {
+                    enumerator.Reset();
+                }
+                currentTarget = enumerator.Current;
+            }
+
+
+            GetComponent<Rigidbody2D>().velocity = delta.normalized * speed;
+        }
+    }
+	}
+
+    // On collision
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector2 direction = transform.position - col.transform.position;
+        StartCoroutine("Bump",direction);
+    }
+
+    private IEnumerator Bump(Vector2 direction)
+    {
+        isBumping = true;
+        GetComponent<Rigidbody2D>().velocity = direction.normalized * speed * 0.5f;
+        yield return new WaitForSeconds(0.3f);
+        isBumping = false;
+    }
+
+    private bool seeHero()
+    {
+        if(!hero)
+        {
+            RaycastHit2D result = Physics2D.CircleCast(transform.position, detectionRadius, transform.forward,detectionRadius,LayerMask.GetMask("Hero"));
+            if (result)
+            {
+                hero = result.collider.gameObject;
+                return true;
+            }else
+            {
+                return false;
+            }
+        } else
+        {
+            return true;
+        }
+    }
+}
